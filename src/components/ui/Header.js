6d3e22ebafb563d0bee6d91c00f5244f0c70418a
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-11 22:35:10 +0530

    First Commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <h2 class="ui header">
+    <img class="ui image" src="https://www.nicepng.com/png/full/62-628999_breaking-bad-logo-png.png"/>
+    <div class="content white">
+      Breaking Bad
+    </div>
+  </h2>
+  )
+}
+
+export default Header;
