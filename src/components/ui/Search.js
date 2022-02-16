cd102484b136131d0a33bd9310dbd28caa1dec5e
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-16 18:55:53 +0530

    changes

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -12,11 +12,15 @@ function Search({getQuery}) {
  
     return (
     <div className="ui segment">
-      <div className="ui fluid action input">
-        <input type=" text" placeholder="Search Characters" value = {text} onChange={e=> onInputChange(e.target.value)} />
-        <button className="ui icon button">
-          <i className="search  icon"></i>
-        </button>
+      <div className="ui fluid icon input">
+      <i className="search icon"></i>
+        <input type=" text" placeholder="Search Characters" value = {text} onChange={e=> onInputChange(e.target.value)}>
+
+       
+        </input>
+       
+        
+        
       </div>
     </div>
   );
